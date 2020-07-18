a7b5cac8acad39ef688e7bbbfed6eaed9d16c072
Pankaj Raturi <panakj@tripatinfoways.com>
2020-07-19 03:54:59 +0530

    Chapter 4, Practice exercises

---
+++ chapter_4/exercise1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+perHourCharges = 0.51
+
+dailyCost = perHourCharges * 24
+monthlyCost = dailyCost * 30
+
+print( 'Cost to operate per day: ${}'. format(dailyCost) )
+print( 'Cost to operate per month: ${}'. format(monthlyCost) )
