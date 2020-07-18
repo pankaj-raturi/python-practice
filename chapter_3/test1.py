0d171cecf0584b7c694b9a0d5c8b1c8df594a37d
Pankaj Raturi <panakj@tripatinfoways.com>
2020-07-19 02:10:47 +0530

    First Practice exercise for chapter 3

---
+++ chapter_3/test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+animal = 'Lion'
+vegetable = 'Tomato'
+mineral = 'Gold'
+
+print('Here is an animal. a vegetable, and a mineral')
+print(animal)
+print(vegetable)
+print(mineral)
