d67bb51f70efe0cee6c862d05043defea0b17af3
Pankaj Raturi <panakj@tripatinfoways.com>
2020-07-21 03:12:16 +0530

    Tuples practice

---
+++ chapter_9/exercise1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+#list of airports
+airports = [
+    ('New Delhi International Airport', 'IXC'),
+    ('Chandigarh International Airport', 'CHD'),
+    ('Amritsar International Airport', 'ASR'),
+]
+
+for airport, code in airports:
+    print('The code for {} is {}'. format(airport, code))
