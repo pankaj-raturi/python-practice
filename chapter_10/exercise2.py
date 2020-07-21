c1c3085f05f1ffac18f3217c4ba0248404d0612e
Pankaj Raturi <panakj@tripatinfoways.com>
2020-07-21 15:22:42 +0530

    File Practice 2

---
+++ chapter_10/exercise2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+animalsList = []
+
+with open('animals.txt', 'r') as animalsFile:
+    for animal in animalsFile:
+        animalsList.append(animal.strip())
+
+#Sort the animalsList
+animalsList.sort()
+
+
+#Create animals-sorted fiel and iterate through the list again to pu the data in
+with open('animals-sorted.txt', 'w') as animalsFile:
+    for animal in animalsList:
+        animalsFile.write('{}\n'. format(animal))
+
+# print(animalsList)
